9d73f645cd190b1b980bab6b73488a064849c82f
terra <tera.g@inqwise.com>
2024-08-27 15:00:22 +0300

    no message

--- parameters_exist.sh
+++ parameters_exist.sh
@@ -19,6 +19,8 @@ echo "Playbook Base URL: $PLAYBOOK_BASE_URL"
 PLAYBOOK_NAME="ansible-openvpn"
 echo "playbook_name: $PLAYBOOK_NAME"
 
+PLAYBOOK_VERSION="latest"
+
 VAULT_PASSWORD="123123"
 
 bash parameters_accepts.sh --get_pip_url "$GET_PIP_URL" --playbook_name "$PLAYBOOK_NAME" --playbook_base_url "$PLAYBOOK_BASE_URL" -r "$REGION" --account_id "$ACCOUNT_ID" --topic_name "$TOPIC_NAME" --vault_password $VAULT_PASSWORD
